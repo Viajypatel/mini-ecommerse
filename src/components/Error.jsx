c46093fcae053d240167b5da85bc1d0fc5e9bfe4
Vijay Patel <vijaypatel114200@gmail.com>
2025-08-27 17:25:38 +0530

    change in error file

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,3 +1,3 @@
 export default function Error({ message }) {
-  return <div className="text-red-500 text-center py-10">{message}</div>;
+  return <div className="text-red-600 text-center py-10">{message}</div>;
 }
